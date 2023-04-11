764da373fada280042c59740afb3af4ea7627b89
Astha47 <anasfathurrahman.edu@gmail.com>
2023-04-11 20:10:01 +0700

    menambah format file function

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 # IMPORT MODULE
 
+# IMPORT FUNCTIONS
+import src.commands.F01
+import src.commands.F02
+import src.commands.F03
+import src.commands.F04
+import src.commands.F05
+import src.commands.F06
+import src.commands.F07
+import src.commands.F08
+import src.commands.F09
+import src.commands.F10
+import src.commands.F11
+import src.commands.F12
+import src.commands.F13
+import src.commands.F14
+import src.commands.F15
+import src.commands.F16
 
 #
+
+src.commands.F01.halo()
