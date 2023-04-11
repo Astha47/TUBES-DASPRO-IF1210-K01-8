232860573194fafa44dcf6f5ebe05b32e0b000d5
Astha47 <anasfathurrahman.edu@gmail.com>
2023-04-11 20:02:07 +0700

    Membuat struktur dasar

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-from data import load
-import commands
+# IMPORT MODULE
 
-# Anggap semua fungsi yang dipanggil merupakan fungsi yang sudah dibuat sendiri pada modul lain
-users = [] # Matriks data user
-candi = [] # Matriks data candi
-bahan_bangunan = [] # Data bahan bangunan
 
-load("file/user.csv", users)
-load("file/candi.csv", candi)
-load("file/bahan_bangunan.csv", bahan_bangunan)
-
-while True:
-  masukan = input(">>> ")
-  commands.run(masukan)
+#
