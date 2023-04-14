f5fe1f0f6429c5d231337738073622ad0b16157d
Salsabilla Putri Aprillia <16522037@mahasiswa.itb.ac.id>
2023-04-14 12:50:07 +0700

    revisi lagi

---
+++ src/commands/F07_JinPengumpul.py
@@ -0,0 +1,38 @@
+#(butuh revisi)
+#def kumpul()
+#    return hasil
+
+#F02 - Kumpul
+
+#SPESIFIKASI DAN DEFINISI
+
+"""
+material : {  pasir : integer
+                batu  : integer
+                air   : integer }
+"""
+
+#function kumpul () -> hasil : string
+#{fungsi ini akan menghasilkan kumpulan material (pasir, batu, dan air) dengan nilai yang random antara 0-5
+#untuk membangun candi dan menyimpan nilai hasil kumpulan material dalam sebuah array bernama 'material'}
+
+#REALISASI
+
+import random
+
+#function kumpul () -> hasil : string
+#Kamus Lokal
+"""
+pasir, batu, air : integer
+material : array of integer
+hasil : string
+"""
+#Algoritma
+def kumpul():
+    pasir = random.randint(0,5) 
+    batu = random.randint(0,5)
+    air = random.randint(0,5)
+    material = [pasir,batu,air]
+    hasil = "Jin menemukan {} pasir, {} batu, dan {} air".format(pasir,batu,air)
+    return hasil
+    
